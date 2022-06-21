5c2fb47b407df2f28ded7ac0cdabed990ee55021
rachelCs2021 <rachelchasha@gmail.com>
2022-06-21 11:28:40 +0300

    start working

---
+++ game.js
@@ -0,0 +1,16 @@
+
+let buttonColours = ["red", "blue", "green", "yellow"]
+
+let gamePattern = []
+
+function nextSequence() {
+
+    let randomNumber = Math.floor(Math.random() * 4)
+
+    let randomChosenColour = buttonColours[randomNumber]
+
+    gamePattern.push(randomChosenColour)
+
+}
+
+$(`#${randomChosenColour}`).delay(100).fadeOut().fadeIn('slow')
