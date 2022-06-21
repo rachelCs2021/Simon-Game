4875c882e5058d287849976fe5b9b08ce7100991
rachelCs2021 <rachelchasha@gmail.com>
2022-06-21 13:30:13 +0300

    add logic

--- game.js
+++ game.js
@@ -2,8 +2,34 @@
 let buttonColours = ["red", "blue", "green", "yellow"]
 
 let gamePattern = []
+let userClickedPattern = []
+
+let started = false;
+let level = 0;
+
+$("body").keypress(function () {
+    if (!started) {
+        $("h1").text(`Level ${level}`);
+        nextSequence();
+        started = true;
+    }
+})
+
+$(".btn").click(function () {
+
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour)
+    animatePress(userChosenColour)
+    checkAnswer(userClickedPattern.length - 1);
+})
 
 function nextSequence() {
+
+    userClickedPattern = []
+    level++
+    $("h1").text(`Level ${level}`);
 
     let randomNumber = Math.floor(Math.random() * 4)
 
@@ -11,6 +37,47 @@ function nextSequence() {
 
     gamePattern.push(randomChosenColour)
 
+    $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour)
 }
 
-$(`#${randomChosenColour}`).delay(100).fadeOut().fadeIn('slow')
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $(`#${currentColour}`).addClass("pressed")
+    setTimeout(function () {
+        $(`#${currentColour}`).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong")
+        $("body").addClass("game-over")
+        $("h1").text("Game Over, Press Any Key to Restart")
+
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver()
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
